664e3f12235218ef7ca7321271be9343b17839fa
stan e <yi.hang@live.com>
2019-12-04 21:49:01 +0800

    增加uni-app的代码

---
+++ uni-app/my-cli-uni-app-project/src/pages/tabbar/tabbar-1/tabbar-1.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="content">
+		页面 - 1
+	</view>
+</template>
+
+<script>
+// import {Tabbar11Component} from './tabbar1-1.component';
+// 
+// export class Tabbar11 extends Tabbar11Component{
+// 	
+// 	constructor(){
+// 		super();
+// 	}
+// 	
+// }
+	
+export default {
+	data() {
+		return {
+			title: 'Hello'
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
+</script>
+
+<style>
+.content {
+	text-align: center;
+	height: 400upx;
+	margin-top: 200upx;
+}
+</style>
